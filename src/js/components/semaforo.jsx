2898b8b27f50442971f1de851e4dc18e06568d7f
Adrianmrc94 <adrianmrc94@gmail.com>
2025-03-10 14:03:14 +0000

    semaforo basico

---
+++ src/js/components/semaforo.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+
+const TrafficLight = () => {
+  const [color, setColor] = useState("red");
+
+  return (
+    <div className="traffic-light-container">
+      <div className="traffic-light">
+        <div 
+          className={`light red ${color === 'red' ? 'active' : ''}`}
+          onClick={() => setColor('red')}
+        ></div>
+        <div 
+          className={`light yellow ${color === 'yellow' ? 'active' : ''}`}
+          onClick={() => setColor('yellow')}
+        ></div>
+        <div 
+          className={`light green ${color === 'green' ? 'active' : ''}`}
+          onClick={() => setColor('green')}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+
+export default TrafficLight;
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,9 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import TrafficLight from './components/semaforo';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+    <TrafficLight/>
+
 )
